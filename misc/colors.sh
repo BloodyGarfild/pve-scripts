79eed909e2f6a5e0da99a81de391bcef6a88caf7
Andre Schiller <130280275+And-rix@users.noreply.github.com>
2025-02-17 14:43:36 +0100

    Create colors.sh

---
+++ misc/colors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Colors
+R='\033[0;31m'  # Red
+G='\033[0;32m'  # Green
+Y='\033[0;33m'  # Yellow
+B='\033[0;34m'  # Blue
+M='\033[0;35m'  # Magenta
+C='\033[0;36m'  # Cyan
+W='\033[0;37m'  # White
+
+# Background-Colors
+BG_R='\033[41m'
+BG_G='\033[42m'
+BG_Y='\033[43m'
+BG_B='\033[44m'
+BG_M='\033[45m'
+BG_C='\033[46m'
+BG_W='\033[47m'
+
+# Reset-Color
+X='\033[0m'
