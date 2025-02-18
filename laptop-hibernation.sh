fe60a713c360616ed34825d62fe7bc1d8e8774f2
Andre Schiller <130280275+And-rix@users.noreply.github.com>
2025-02-18 11:44:41 +0100

    Update laptop-hibernation.sh

--- laptop-hibernation.sh
+++ laptop-hibernation.sh
@@ -18,24 +18,25 @@ echo""
 # Ask for continue script
 echo -e "${Y}This script will add the required lines into${X}"
 echo "-----"
-echo -e "${R}'/etc/systemd/logind.conf'${X}"
+echo -e "${R}${INFO}'/etc/systemd/logind.conf'${X}"
 echo "-----"
 echo -e "${Y}to use a laptop as an PVE host${X}"
 echo ""
-echo -e "${Y}Run script now? (y/N)${X}"
+echo -e "${Y}${INFO}Run script now? (y/N)${X}"
 read run_script
 echo ""
 
 if [[ "$run_script" =~ ^[Yy]$ ]]; then
-		echo -e "${G}Running...${X}"
+		echo -e "${G}${OK}Running...${X}"
+  		echo ""
     else
-		echo -e "${R}Stopping...${X}"
+		echo -e "${R}${NOTOK}Stopping...${X}"
 		exit 1
 fi
 
 # Ensure the script is run as root
 if [[ $EUID -ne 0 ]]; then
-    echo "${R}⚠️ This script must be run as root!${X}"
+    echo "${R}${WARN} This script must be run as root!${X}"
     exit 1
 fi
 
@@ -43,6 +44,7 @@ echo -e "${G}Configuring Proxmox for laptop usage...${X}"
 
 # Disable hibernation and suspend when closing the lid
 echo -e "${Y}Disabling hibernation and suspend on lid close...${X}"
+echo ""
 
 CONFIG_FILE="/etc/systemd/logind.conf"
 
@@ -55,11 +57,11 @@ sed -i '/^HandleLidSwitchExternalPower=/d' $CONFIG_FILE
 sed -i '/^HandleLidSwitchDocked=/d' $CONFIG_FILE
 
 # Apply new settings
-echo "HandleLidSwitch=ignore" >> $CONFIG_FILE
+echo -e "HandleLidSwitch=ignore" >> $CONFIG_FILE
 echo "HandleLidSwitchExternalPower=ignore" >> $CONFIG_FILE
 echo "HandleLidSwitchDocked=ignore" >> $CONFIG_FILE
 
 # Restart systemd-logind to apply changes
 systemctl restart systemd-logind
 
-echo -e "${G}✅ Proxmox laptop configuration completed!${X}"
+echo -e "${G}${OK} Proxmox laptop configuration completed!${X}"
