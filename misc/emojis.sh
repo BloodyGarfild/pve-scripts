0b2a0ad9f9fb96b5f3bc5b62ac31ae5ce4747301
Andre <130280275+And-rix@users.noreply.github.com>
2025-02-24 09:32:26 +0100

    Update emojis.sh

--- misc/emojis.sh
+++ misc/emojis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 TAB="  "
 INFO="${TAB}ℹ️${TAB}${CL}"
+START="${TAB}▶️${TAB}${CL}"
 OK="${TAB}✅${TAB}${CL}"
 NOTOK="${TAB}❌${TAB}${CL}"
 WARN="${TAB}⚠️${TAB}${CL}"
 DISK="${TAB}💾${TAB}${CL}"
+
+
+
